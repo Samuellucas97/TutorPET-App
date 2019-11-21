df627130a497378f31d20cad8b1e016079150e67
Your Name <you@example.com>
2019-11-21 16:38:12 -0300

    Atualizando master

--- TutorPET/app/src/main/java/com/example/tutorpet/ui/tutorias/TutoriasFragment.kt
+++ TutorPET/app/src/main/java/com/example/tutorpet/ui/tutorias/TutoriasFragment.kt
@@ -10,6 +10,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.tutorpet.R
 import com.example.tutorpet.adapter.TutoriaAgendadaAdapter
 import com.example.tutorpet.model.TutoriaAgendada
@@ -106,7 +107,7 @@ class TutoriasFragment : Fragment() {
                 }
 
                 rvMonitorias.adapter = monitoriaAgendadaAdapter
-                rvMonitorias.layoutManager = LinearLayoutManager(context)
+                rvMonitorias.layoutManager = LinearLayoutManager(context) as RecyclerView.LayoutManager?
 
             }
             .addOnFailureListener{e->
