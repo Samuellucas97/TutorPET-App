7e4b49e0a1575213f53e7ac93437e84da64fe667
Your Name <you@example.com>
2019-11-25 00:04:43 -0300

    Iniciando migração

---
+++ TutorPET/app/src/main/java/com/example/tutorpet/model/Usuario.kt
@@ -0,0 +1,4 @@
+package com.example.tutorpet.model
+
+class Usuario {
+}
--- TutorPET/app/src/main/java/com/example/tutorpet/view/LoginActivity.kt
+++ TutorPET/app/src/main/java/com/example/tutorpet/view/LoginActivity.kt
@@ -3,18 +3,46 @@ package com.example.tutorpet
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.tutorpet.view.HomeActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        auth = FirebaseAuth.getInstance()
 
         btnEntrar.setOnClickListener {
-            startActivity(Intent(this, HomeActivity::class.java))
+
+            if (edtEmail.text.isEmpty() ) {
+                edtEmail.error =  "Campo E-mail Obrigatório"
+
+            }
+            else if (edtSenha.text.isEmpty()) {
+                edtSenha.error =  "Campo Senha Obrigatório"
+            }
+            else {
+
+                auth.signInWithEmailAndPassword(edtEmail.text.toString(), edtSenha.text.toString())
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            startActivity(Intent(this, HomeActivity::class.java))
+                        } else {
+                            Toast.makeText(
+                                this@LoginActivity,
+                                "Autenticação Falhou!!",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
+
+                    }
+            }
         }
 
 
